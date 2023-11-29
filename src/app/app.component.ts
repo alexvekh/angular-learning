5bb4d370dffc6f438ea9873e4ea44a6c623b9f00
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2023-11-28 19:57:12 -0500

    2 way binding with imported FormsModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
 import { CardComponent } from './card/card.component';
 import { FormComponent } from './form/form.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CardComponent, FormComponent],
+  imports: [CommonModule, RouterOutlet, CardComponent, FormComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from "@angular/core";
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-card',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
@@ -18,9 +21,12 @@ export class CardComponent implements OnInit {
     this.title = "Title has been changed!"
   }
 
-  inputHandler(event: any) {
-    const value = event.target.value
-    this.title = value
+  changeHandler() {
+    console.log(this.title)
   }
 
+  // inputHandler(value: any) {
+  //   //const value = event.target.value
+  //   this.title = value
+  // }
 }
