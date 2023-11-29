109149b5368f72ddac3017e9ff7a43632f9c69a9
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2023-11-29 10:09:20 -0500

    title color and toggle cards

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { FormsModule } from '@angular/forms';
+import { NgStyle } from "@angular/common";
+import { NgClass } from "@angular/common";
 
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgStyle, NgClass],
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
@@ -13,6 +15,8 @@ import { FormsModule } from '@angular/forms';
 export class CardComponent implements OnInit {
   title = "My Card Tittle"
   text = "My simple text"
+
+  textColor: string = ''
 
   ngOnInit() {
   }
